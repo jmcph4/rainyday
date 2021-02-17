64b9c2ea4bcdc986c1e9d1ada3d51c5f68e7bb52
jmcph4 <jmcph4.github@gmail.com>
2021-02-17 17:39:06 +1000

    Implement command-line parsing
    This commit defines the command-line interface via `clap`.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use clap::{App, Arg, ArgMatches};
+
 fn main() {
-    println!("Hello, world!");
+    let _matches: ArgMatches = App::new("rainyday")
+        .version("0.0.1")
+        .arg(Arg::with_name("input_file").required(true))
+        .get_matches();
 }
