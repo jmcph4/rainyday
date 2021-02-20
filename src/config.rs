e04173c079a35036a2f8a2c44ee5c9f08f6dee7c
jmcph4 <jmcph4.github@gmail.com>
2021-02-20 18:15:27 +1000

    Implement reading from config file
    This commit implements reading from a configuration file.

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,7 @@
+use std::convert::TryFrom;
+use std::fs::File;
+use std::io::{BufReader, Read};
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -11,6 +15,24 @@ impl Default for Config {
         Config {
             pedantic: false,
             respect_private_trackers: true,
+        }
+    }
+}
+
+impl TryFrom<File> for Config {
+    type Error = String;
+
+    fn try_from(value: File) -> Result<Self, Self::Error> {
+        let mut contents: String = String::new();
+        let mut buf: BufReader<File> = BufReader::new(value);
+        match buf.read_to_string(&mut contents) {
+            Ok(t) => t,
+            Err(e) => return Err(format!("{}", e)),
+        };
+
+        match toml::from_str(&contents) {
+            Ok(t) => t,
+            Err(e) => Err(format!("{}", e)),
         }
     }
 }
