f6fedcd51afaa3f019362dbe5a9ff52dcf5aad2a
jmcph4 <jmcph4.github@gmail.com>
2021-02-27 22:13:57 +1000

    Implement decoding for `HavePayload`
    This commit implements decoding `Have` peer messages by implementing
    the `TryFrom` trait for the `HavePayload` type.

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,7 +1,11 @@
 #![allow(dead_code)]
+use std::convert::TryFrom;
 use std::fmt::Display;
+use std::mem::size_of;
 
 use thiserror::Error;
+
+type Bytes = Vec<u8>;
 
 #[derive(Debug, Display, PartialEq, Eq, Error)]
 pub enum DecodeError {
@@ -11,7 +15,26 @@ pub enum DecodeError {
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct HavePayload {
-    index: usize,
+    index: u32,
+}
+
+impl TryFrom<Bytes> for HavePayload {
+    type Error = DecodeError;
+
+    fn try_from(value: Bytes) -> Result<Self, Self::Error> {
+        if value.len() > size_of::<u32>() {
+            return Err(Self::Error::TooLong);
+        }
+
+        if value.len() < size_of::<u32>() {
+            return Err(Self::Error::TooShort);
+        }
+
+        let bytes_array: [u8; 4] = [value[0], value[1], value[2], value[3]];
+        Ok(Self {
+            index: u32::from_be_bytes(bytes_array),
+        })
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
