61ec116559f0f31904c159a4051bfab917361325
jmcph4 <jmcph4.github@gmail.com>
2021-02-27 22:20:14 +1000

    Implement decoding for `RequestPayload`
    This commit implements decoding `Request` peer messages by implementing
    the `TryFrom` trait for the `RequestPayload` type.

--- src/protocol.rs
+++ src/protocol.rs
@@ -44,9 +44,33 @@ pub struct BitfieldPayload {
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct RequestPayload {
-    index: usize,
-    begin: usize,
-    length: usize,
+    index: u32,
+    begin: u32,
+    length: u32,
+}
+
+impl TryFrom<Bytes> for RequestPayload {
+    type Error = DecodeError;
+
+    fn try_from(value: Bytes) -> Result<Self, Self::Error> {
+        if value.len() > 3 * size_of::<u32>() {
+            return Err(Self::Error::TooLong);
+        }
+
+        if value.len() < 3 * size_of::<u32>() {
+            return Err(Self::Error::TooShort);
+        }
+
+        let index_bytes: [u8; 4] = [value[0], value[1], value[2], value[3]];
+        let begin_bytes: [u8; 4] = [value[4], value[5], value[6], value[7]];
+        let length_bytes: [u8; 4] = [value[8], value[9], value[10], value[11]];
+
+        Ok(Self {
+            index: u32::from_be_bytes(index_bytes),
+            begin: u32::from_be_bytes(begin_bytes),
+            length: u32::from_be_bytes(length_bytes),
+        })
+    }
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
