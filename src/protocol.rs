f991b379350485abf28e398b171cd0bdb6cd6aa1
jmcph4 <jmcph4.github@gmail.com>
2021-02-27 20:37:56 +1000

    Derive C-COMMON-TRAITS traits for message types
    This commit implements the (relevant) traits from the Rust API
    Guidelines' C-COMMON-TRAITS section, via derive macros.

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,31 +1,37 @@
 #![allow(dead_code)]
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct HavePayload {
     index: usize,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct BitfieldPayload {
     bitfield: Vec<u8>,
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct RequestPayload {
     index: usize,
     begin: usize,
     length: usize,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct PiecePayload {
     index: usize,
     begin: usize,
     piece: Vec<u8>,
 }
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct CancelPayload {
     index: usize,
     begin: usize,
     length: usize,
 }
 
+#[derive(Clone, Debug, PartialEq, Eq)]
 enum PeerMessage {
     Choke,
     Unchoke,
